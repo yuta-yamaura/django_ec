9fd32659cd59e53beaa67e40a4e23e429503be05
yuta-yamaura <shk0fate@gmail.com>
2024-05-28 09:48:28 +0900

    settings.pyから「import django_heroku と django_heroku.setting(locals())」を削除(初回のみのgit pushで良いため)

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import environ
 
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
