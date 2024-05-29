2f7b32756701e06981d40143f59ff3d23bc75928
yuta-yamaura <shk0fate@gmail.com>
2024-05-29 12:41:16 +0900

    settings.pyのDATABASESの部分を修正

--- config/settings.py
+++ config/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(default=env('DATABASE_URL')),
-}
-
+# DATABASES = {
+#     "default": dj_database_url.config(default=env('DATABASE_URL')),
+# }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True),
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
