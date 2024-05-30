f89d6d1c2672c22be8474dec5e674f97915a2f90
yuta-yamaura <shk0fate@gmail.com>
2024-05-30 11:00:09 +0900

    Dockerfileとsettings.pyを修正

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
-CMD gunicorn django2ec.wsgi --log-file -
+CMD gunicorn django2ec.wsgi --log-file -,python3 manage.py runserver 0.0.0.0:$PORT
--- config/settings.py
+++ config/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     "default": dj_database_url.config(default=env('DATABASE_URL')),
 }
 
+DATABASES['default'] = dj_database_url.config(default='DATABASE_URL')
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
