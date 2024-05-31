04e071349836726621cd40300e71f6fae53a575e
yuta-yamaura <shk0fate@gmail.com>
2024-05-31 12:06:31 +0900

    settings.pyのDB設定の修正とマイグレーションファイルを生成

--- config/settings.py
+++ config/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": dj_database_url.config(default=env('DATABASE_URL')),
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    "default": dj_database_url.config(default=env('DATABASE_URL')),
 }
 
 # Password validation
