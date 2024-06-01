a8b49feef2fa07d6d8a73416ad411858faa19de1
yuta-yamaura <shk0fate@gmail.com>
2024-06-01 12:05:19 +0900

    settings.pyにAWSのアクセスきーを環境変数として設定

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,9 @@ environ.Env.read_env(env_file=str(BASE_DIR) + "/.env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
+AWS_ACCESS_KEY_ID = env("SECRET_KEY")
+AWS_SECRET_ACCESS_KEY = env("AWS_STORAGE_BUCKET_NAME")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
