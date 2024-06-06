556816b2865bf751754be6a2b3e22f231a869b35
yuta-yamaura <shk0fate@gmail.com>
2024-06-06 12:02:09 +0900

    管理画面を更新

--- ec/admin.py
+++ ec/admin.py
@@ -9,5 +9,8 @@ class Postadmin(admin.ModelAdmin):
     list_display = ('image', 'name', 'price', 'created_at', 'updated_at', 'discription')
     list_display_links = ('image',)
     list_filter = ('name', 'price',)
+    search_fields = ('name', 'price',)
+    list_per_page = 4
+    ordering = ('created_at', )
 
 admin.site.register(ProductModel, Postadmin)
