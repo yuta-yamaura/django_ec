35aa9528c1c21e2830211e78cbd90563e4e388b9
yuta-yamaura <shk0fate@gmail.com>
2024-07-01 21:45:42 +0900

    settinds.pyのSTATIC_ROOTを修正

--- config/settings.py
+++ config/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = 'static/prd/'
+STATIC_ROOT = 'static/'
 
 STATIC_URL = 'static/'
 
