7606c31cf90bd66fad92793b8b1a2aa93ee4e928
yuta-yamaura <shk0fate@gmail.com>
2024-07-14 14:50:13 +0900

    settings.pyを修正

--- config/settings.py
+++ config/settings.py
@@ -161,18 +161,12 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = S3_URL
 
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
-
-    STORAGES = {
-        # "default": {
-        #     "BACKEND": "storages.backends.s3.S3Storage",
-        # },
-        'staticfiles': {
-            'BACKEND': 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',
-        },
-    }
 
     SECRET_KEY = os.environ['SECRET_KEY']
     import django_heroku
