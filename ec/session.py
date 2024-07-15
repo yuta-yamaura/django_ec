57c5889d03b67fc6a3b71d3755bc5666b3803692
yuta-yamaura <shk0fate@gmail.com>
2024-07-15 23:08:13 +0900

    session.pyの条件分岐を修正

---
+++ ec/session.py
@@ -0,0 +1,15 @@
+from ec.models import CartModel
+
+
+def get_session(request):
+    cart_id = request.session.get('cart_id', None)
+    if cart_id is None:
+        cart, created  = CartModel.objects.get_or_create(
+            cart_id=cart_id
+            )
+        request.session['cart_id'] = cart.cart_id
+        return cart
+    cart, created = CartModel.objects.get_or_create(
+        cart_id=cart_id
+        )
+    return cart
